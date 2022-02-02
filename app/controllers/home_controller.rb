e4d71c4e4c3a043317eda4d56ca7890b5e7a35d1
Victor Basantes <victor_basantes@hotmail.com>
2022-02-02 17:31:57 -0500

    Add home route condition if user is signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
 
   def index
+    if user_signed_in?
+      redirect_to controller: :friends, action: :index
+    end
   end
 
   def about
   end
-  
+
 end
